473fe246bd54c844225912f612cb44fe2a23dfe0
Spencer Scarlett <Spencer.Scarlett@stu.ocsb.ca>
2022-09-18 21:32:47 +0000

    Added area_per_reat.py

---
+++ area_per_rect.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+# Created by: Spencer.S
+# Created on: Sept 18, 2022
+# This program calculates and displays the area and perimeter of a 
+# rectangle with length 10cm and width 4cm.
+
+def main():
+    print ("A rectangle has the dimensions of")
+    print ("A length of 10cm and width of 4cm:")
+    print ("")
+    print ("Area = {} cm^2". format (10*4))
+    print ("Perimeter = {} cm". format (2*(10+4)))
+
+
+if __name__ == "__main__":
+  main()
---
+++ basic_math.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+# Created by: Spencer.Scarlett
+# Created on: Sept 18, 2022
+# This program solves basic math questions
+
+def main():
+  # The function answers basic questions
+    print ("5 + 3 = {}". format(5+3))
+    print ("9 - 6 = {}". format(9-6))
+    print ("6 / 3 = {}". format(6/3))
+    print ("6 - 3 / 2 = {}". format(6-3/2))
+    print ("6^2 = {}". format(6**2))
+    print ("4 / 2 = {}". format(4/2))
+
+
+if __name__ == "__main__":
+  main()
